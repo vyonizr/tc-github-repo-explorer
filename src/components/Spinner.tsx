2672cebfe5599c42972b83ddc2e59cb019fa6fb0
Fitrahtur Rahman <fitrahtur@gmail.com>
2023-06-14 08:39:51 +0700

    make spinner smaller

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -3,7 +3,7 @@ function Spinner() {
     <>
       <svg
         aria-hidden='true'
-        className='w-8 h-8 mr-2 text-blue-400 animate-spin  fill-white'
+        className='w-6 h-6 text-blue-400 animate-spin  fill-white'
         viewBox='0 0 100 101'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
